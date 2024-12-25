816255003b729b62137ba50a0e0c21d84fe7d690
AyaanSayed <138655396+AyaanSayed@users.noreply.github.com>
2024-12-25 22:31:37 +0530

    updated form

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
+
 export default function Main() {
+  const [ingredients, setIngredients] = useState(["salt", "pepper", "oregano", "thyme", "rosemary"]);
+  const ingredientsList = ingredients.map((ingredient) => {
+      return <li key={ingredient}> {ingredient}</li>
+  });
+
+
+
+  function addIngredient(formData){
+    const newIngredient = formData.get("ingredient");
+    setIngredients(prevIngredients => ([...prevIngredients, newIngredient]));
+  }
+
   return (
       <main>
-          <form className="add-ingredient-form">
+          <form className="add-ingredient-form" action={addIngredient}>
               <input 
                   type="text"
                   placeholder="e.g. oregano"
                   aria-label="Add ingredient"
+                  name="ingredient"
               />
               <button>Add ingredient</button>
           </form>
+          <ul>
+                  {ingredientsList}
+          </ul>
       </main>
   )
 }

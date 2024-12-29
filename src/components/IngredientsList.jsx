99b40dfbb7b97b0e1bf9f56e73450211df7732f8
AyaanSayed <138655396+AyaanSayed@users.noreply.github.com>
2024-12-29 16:44:20 +0530

    used useEffect

--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -6,7 +6,7 @@ export default function IngredientsList(props) {
   return <section>
     <h2>Ingredients on hand:</h2>
     <ul className="ingredients-list" aria-live="polite">{ingredientsList}</ul>
-    {ingredientsList.length > 3 && <div className="get-recipe-container">
+    {ingredientsList.length > 3 && <div ref={props.ref} className="get-recipe-container">
       <div>
         <h3>Ready for a recipe?</h3>
         <p>Generate a recipe from your list of ingredients.</p>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ClaudeRecipe } from "./ClaudeRecipe";
 import IngredientsList from "./IngredientsList";
 import { getRecipeFromMistral } from "../ai";
@@ -6,7 +6,13 @@ import { getRecipeFromMistral } from "../ai";
 export default function Main() {
     const [ingredients, setIngredients] = useState([]);
     const [recipe, setRecipe] = useState("");
+    const recipeSection = useRef(null);
 
+    useEffect(() => {
+        if (recipe !== "" && recipeSection.current !== null) {
+            recipeSection.current.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [recipe]);
 
 
 
@@ -31,7 +37,7 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            {ingredients.length > 0 && <IngredientsList ingredients={ingredients} getRecipe={getRecipe}/>}
+            {ingredients.length > 0 && <IngredientsList ref={recipeSection} ingredients={ingredients} getRecipe={getRecipe}/>}
 
             {recipe && <ClaudeRecipe recipe={recipe}/>}
 
